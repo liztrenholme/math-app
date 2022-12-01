60e03c4419c456cc696a7497b5b26810fec7e35d
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2022-12-01 01:41:32 -0500

    Updates

---
+++ precache-manifest.c8a1c662174b8e58589de41d3d5211dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf9da37f39fcbcd40deed3ea208d1d4",
+    "url": "/math-app/index.html"
+  },
+  {
+    "revision": "0d1aa3e6112ee1f03340",
+    "url": "/math-app/static/css/main.f18bfd37.chunk.css"
+  },
+  {
+    "revision": "248230548173ffb48f41",
+    "url": "/math-app/static/js/2.fa7177b7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/math-app/static/js/2.fa7177b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d1aa3e6112ee1f03340",
+    "url": "/math-app/static/js/main.6f1755d0.chunk.js"
+  },
+  {
+    "revision": "f31ea49037ebcf3ce0fb",
+    "url": "/math-app/static/js/runtime-main.8a282f4b.js"
+  },
+  {
+    "revision": "d6142527d9b1df78c1ec44ae2a16987e",
+    "url": "/math-app/static/media/infoicon.d6142527.png"
+  }
+]);
